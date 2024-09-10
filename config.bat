b919e6a185ab8752e0486e5f9799aee4aa383d89
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2024-09-10 15:37:06 -0400

    Update config.bat

--- config.bat
+++ config.bat
@@ -6,7 +6,7 @@ REM Run the Python script and check for errors
 echo Running config.py...
 python config.py
 if %errorlevel% neq 0 (
-    echo Error: mouse.py did not run successfully.
+    echo Error: config.py did not run successfully.
     popd
     exit /b %errorlevel%
 )
